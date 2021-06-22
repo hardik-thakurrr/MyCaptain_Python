c8eaf024ec0f83b8acb3748591449d0343847057
Hardik Thakur <66474341+hardik-thakurrr@users.noreply.github.com>
2021-06-22 21:08:41 +0530

    Created Task1_AreaofCircle.py

---
+++ Task1_AreaofCircle.py
@@ -0,0 +1,6 @@
+# Write a Python program which accepts the radius of a circle from the user and computes area
+import math
+radius = float(input("Enter Radius of the Circle -> "))
+area = math.pi*radius**2
+print("\nRadius of the Circle is -> ", radius)
+print("The Area of the Circle is -> ", area)
